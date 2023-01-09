ac765f12c7ae5891733b349b1aa680bc0edbaed7
ElsesserMatias <elsesser3@gmail.com>
2023-01-09 13:49:17 -0300

    Dia Lunes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './pure/greeting';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Mundo!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={logo} className="App-logo" alt="logo" />
+      <Greeting
+      name="Matias Fabian"
+      />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
       </header>
     </div>
-  );
+);
 }
 
 export default App;
---
+++ src/pure/greeting.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Greeting extends Component {
+      constructor(props){// props es como un parametro o propedad que usaria un elemento HTML
+            super(props)// ponemos super para trabajar con las pripiedades que vengan del componente
+            this.state = {// el state es informacion privada del componente, son como propiedades de la clase
+                  age: 25
+            }
+      }
+      
+      
+      render() {
+            return (// SOLO PUEDE DEVOLVER UN UNICO ELEMENTO PADRE
+                  <div>
+                        <h1>
+                              {/* Aqui usamos props ya que le vamos a pedir al usuario del componente un name, que va a poner como propiedad del elemento html*/ }
+                              Hola {this.props.name} !
+                        </h1>
+                        <h2>
+                        {/*aqui accedemos al state del elemento */}
+                              Tu edad es de:  {this.state.age}
+                        </h2>
+                  </div>
+            );
+      }
+}
+
+
+Greeting.propTypes = {
+
+};
+
+
+export default Greeting;
