26ffa42c7ca124007f4556fd0f74dbb2bb6b2bc3
ElsesserMatias <elsesser3@gmail.com>
2023-01-12 19:48:49 -0300

    Clase jueves 12

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import TaskListComponent from './componentss/container/task_list';
-import ComponentA from './componentss/ejercicios/componentA';
+import Ejemplo2 from './hooks/Ejemplo2';
+// import TaskListComponent from './componentss/container/task_list';
+// import ComponentA from './componentss/ejercicios/componentA';
+// import Ejemplo1 from './hooks/Ejemplo1';
 // import Greeting from './pure/greeting';
 // import GreetinF from './componentss/pure/greetinF';
 
@@ -19,13 +21,20 @@ function App() {
       /> */}
 
       {/* componente listado de tareas */}
-      <TaskListComponent/>
-      <ComponentA
+      {/* <TaskListComponent/> */}
+
+      {/* componente ejercicio 1 */}
+      {/* <ComponentA
         name="Matias"
         apellido="Elsesser"
         email="elsesser3@gmail.com"
         conected="true"
-      />
+      /> */}
+
+
+      {/* Ejemplos de uso de HOOKS */}
+      {/* <Ejemplo1/> */}
+      <Ejemplo2/>
       </header>
     </div>
 );
---
+++ src/hooks/Ejemplo2.jsx
@@ -0,0 +1,72 @@
+/**
+Ejemplo de uso de:
+      - useState()
+      - useRef()
+      - useEffect()
+*/
+
+import React, { useState, useRef, useEffect } from "react";
+
+const Ejemplo2 = () => {
+
+      // Vamos a crear dos contadores distintos
+      // Cada uno en un estado diferente
+      const [contador1, setContador1] = useState(0);
+      const [contador2, setContador2] = useState(0);
+
+      //Creamos una referencia con useReff para asociar una variable con un elemento del DOM
+      const miRef = useRef();
+
+      function incrementar1(){
+            setContador1(contador1 + 1);
+      }
+
+      function incrementar2 () {
+            setContador2(contador2 + 1);
+      }
+
+
+      // Trabajamos con useEffect
+      /* ? Caso 1: Ejecutrar SIEMPRE un snippet de codigo ( Cada vez que haya un cambio en el estado del componente se ejecuta lo que este en el useEffect )
+      */
+      // useEffect(() => {
+      //       console.log("Cambio en el estado del componente")
+      //       console.log("Mostrando referencia a elemento del DOM")
+      //       console.log(miRef)
+      // });
+
+      // ? Caso 2: Ejecutar solo cuando cambie contador 1 (Cada vez que haya un cambio en contador 1 se ejecuta lo que diga el useEffect)
+
+      // useEffect recibe primero un callback y como segundo parametro las dependencias, por eso al agregar contador1 solo se ejecutara el useEffect cuando dicha dependencia sufra cambios
+      // useEffect(() => {
+      //       console.log("Cambio de contador 1");
+      //       console.log("Mostranfo referencia a elemento del DOM  ")
+      // }, [contador1]);
+
+      // ? Caso 2: Ejecutar solo cuando cambie contador 1 o 2 (Cada vez que haya un cambio en contador 1 o 2se ejecuta lo que diga el useEffect)
+      useEffect(() => {
+            console.log("Cambio de contador 1");
+            console.log("Mostranfo referencia a elemento del DOM  ")
+      }, [contador1, contador2]);
+
+
+      return (
+            <div>
+                  <h1> *** Ejemplo de useState( ), useRef( ), y useEffect( ) ***</h1>
+                  <h2>CONTADOR 1: { contador1 }</h2>
+                  <h2>CONTADOR 2: { contador2 }</h2>
+
+                  {/* Elemento referenciado */}
+                  <h4 ref={miRef}>
+                        Ejemplo de elemento refereciado
+                  </h4>
+                  {/* Botones para cambiar los contadores */}
+                  <div>
+                        <button onClick={incrementar1}> Incrementar contador 1 </button>
+                        <button onClick={incrementar2}> Incrementar contador </button>
+                  </div>
+            </div>
+      );
+}
+
+export default Ejemplo2;
